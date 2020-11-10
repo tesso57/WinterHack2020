2277f32084deaf89f08175c0bbcd7c833af31811
hatanaka <hatawaniwani24@gmail.com>
2020-11-10 16:45:53 +0900

    poteti test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>src/App.js</code> poteti
         </p>
         <a
           className="App-link"
@@ -23,3 +23,4 @@ function App() {
 }
 
 export default App;
+
