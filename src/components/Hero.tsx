686da4e113c6c835c5f5e209b02df524b34fd1f8
tesso <ta4780@icloud.com>
2020-11-12 23:41:44 +0900

    簡単なHeroコンポーネントを作成

---
+++ src/components/Hero.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+}
+
+const Hero = (props: Props) => {
+    return(
+        <>
+            <h1>Hello</h1>
+        </>
+    )
+}
+
+export default Hero;
