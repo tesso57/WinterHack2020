52fac1f4e538b36841e86f8ccf451ac8abcd486b
hatanaka <hatawaniwani24@gmail.com>
2020-11-13 23:49:49 +0900

    buttonのコンポーネント見た目を作った

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Hero from './components/Hero';
+import RecallButton from './components/RecallButton';
 
 function App() {
   return (
     <>
         <Hero text={"Hello"} bigger={true} />
+        <RecallButton/>
     </>
   );
 }
