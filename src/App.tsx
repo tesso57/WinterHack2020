3b826b1ea984bd26f97ba11896045eaecd3ab287
hatanaka <hatawaniwani24@gmail.com>
2020-11-13 21:52:27 +0900

    邪魔なファイルとコード消した

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import Hero from './components/Hero';
 
 function App() {
   return (
-    <div className="App">
+    <>
         <Hero text={"Hello"} bigger={true} />
-    </div>
+    </>
   );
 }
 
