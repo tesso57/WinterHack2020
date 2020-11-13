f91b280c1466f0cd6ad967047ce836a9c6fdb300
hatanaka <hatawaniwani24@gmail.com>
2020-11-13 20:21:00 +0900

    App.tsxの中をheroだけにした

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,7 @@ import Hero from './components/Hero';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
         <Hero text={"Hello"} bigger={true} />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
